d4042788f145554001fbc9c68cfad9e0e6e07132
kigursjanis <125827153+kigursjanis@users.noreply.github.com>
2023-03-06 00:43:18 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,10 +2,10 @@ import sys
 import threading
 
 
-def calculate_tree_height(n, parents):
-    children = [[] for _ in range(n)]
-    for i in range(n):
-        parent = parents[i]
+def calculate_tree_height(num_nodes, node_parents):
+    children = [[] for _ in range(num_nodes)]
+    for i in range(num_nodes):
+        parent = node_parents[i]
         if parent == -1:
             root = i
         else:
@@ -24,26 +24,25 @@ def calculate_tree_height(n, parents):
 
 
 def main():
-    input_type = input()
+    input_type = input().strip()
 
     if 'I' in input_type:
-        num_nodes = int(input())
-        node_parents = list(map(int, input().split()))
+        num_nodes = int(input().strip())
+        node_parents = list(map(int, input().strip().split()))
         tree_height = calculate_tree_height(num_nodes, node_parents)
         print(tree_height)
     elif 'F' in input_type:
-        file_name = input()
+        file_name = input().strip()
         with open("test/" + file_name, 'r') as f:
-            num_nodes = int(f.readline())
-            node_parents = list(map(int, f.readline().split()))
+            num_nodes = int(f.readline().strip())
+            node_parents = list(map(int, f.readline().strip().split()))
             tree_height = calculate_tree_height(num_nodes, node_parents)
             print(tree_height)
     else:
         print("invalid input")
         exit()
 
-
-
-sys.setrecursionlimit(10**7) 
-threading.stack_size(2**27)  
-threading.Thread(target=main).start()
+if __name__ == '__main__':
+    sys.setrecursionlimit(10**7) 
+    threading.stack_size(2**27)  
+    threading.Thread(target=main).start()
