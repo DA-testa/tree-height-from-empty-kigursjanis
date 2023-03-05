2092cb432059b03bb54be51049e37004c5868195
kigursjanis <125827153+kigursjanis@users.noreply.github.com>
2023-03-06 00:18:42 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,56 +1,46 @@
+# python3
+
 import sys
 import threading
-import numpy
+
 
 def compute_height(n, parents):
-    tree = {}
-    for node, parent in enumerate(parents):
-        if parent == -1:
-            continue
-        if parent not in tree:
-            tree[parent] = [node]
-        else:
-            tree[parent].append(node)
-
     def height(node):
-        if node not in tree:
-            return 1
-      
-        heights = [height(child) for child in tree[node]]
- 
-        return max(heights) + 1
-    root = parents.index(-1)
-    return height(root)
+        if node not in cache:
+            if parents[node] == -1:
+                cache[node] = 1
+            else:
+                cache[node] = 1 + height(parents[node])
+        return cache[node]
+
+    cache = {}
+    return max(height(node) for node in range(len(parents)))
+    
+
 
 def main():
-    print("Input from k(keyboard) or k(file)? (k/f)")
-    source = input().strip().lower()
-    while source not in ['k', 'f']:
-        print("Input error")
-        source = input().strip().lower()
-        if source == 'k':
-            print("Number of nodes:")
-        n = int(input())
+    # newInput = input("Ievadiet 'I' ja vēlaties ievadīt no tastatūras un 'F', ja ieevade no faila:")
+    while True:
+        newInput = input().strip().upper()
+        if newInput == "I":
+            n = int(input())
+            parents = list(map(int,input().split()))
+            break
+        elif newInput == "F":
+            fileName = input()
+            if "a" in fileName:
+                return 1
+            try:
+                with open(fileName) as file:
+                    n = int(file.readline())
+                    parents = list(map(int, file.readline().split()))
+                    break
+            except FileNotFoundError:
+                return 1 
 
-        print("Parents of nodes")
-        parents = list(map(int, input().split()))
-    else:
-        print("Filename:")
-        filename = input().strip()
-        if 'a' in filename:
-            print("Error retrieving filename")
-            return
-        try:
-            with open(f'folder/{filename}', 'r') as f:
-               
-                n = int(f.readline().strip())
-               
-                parents = list(map(int, f.readline().strip().split()))
-        except:
-            print("Error reading file.")
-            return
-    print("Tree Height:", compute_height(n, parents))
+    height = compute_height(n , parents)
+    print(height)            
 
-sys.setrecursionlimit(10**7)  
-threading.stack_size(2**27)   
-threading.Thread(target=main).start() 
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
