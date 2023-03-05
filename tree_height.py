085041191682a6bd034a037cdf3e2f64237f0096
kigursjanis <125827153+kigursjanis@users.noreply.github.com>
2023-03-06 00:07:45 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,56 @@
-# python3
-
 import sys
 import threading
 import numpy
 
-
 def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+    tree = {}
+    for node, parent in enumerate(parents):
+        if parent == -1:
+            continue
+        if parent not in tree:
+            tree[parent] = [node]
+        else:
+            tree[parent].append(node)
 
+    def height(node):
+        if node not in tree:
+            return 1
+      
+        heights = [height(child) for child in tree[node]]
+ 
+        return max(heights) + 1
+    root = parents.index(-1)
+    return height(root)
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    print("Input from K(keyboard) or F(file)? (K/F)")
+    source = input().strip().lower()
+    while source not in ['k', 'f']:
+        print("Input error")
+        source = input().strip().lower()
+        if source == 'k':
+            print("Number of nodes:")
+        n = int(input())
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+        print("Parents of nodes")
+        parents = list(map(int, input().split()))
+    else:
+        print("Filename:")
+        filename = input().strip()
+        if 'a' in filename:
+            print("Error retrieving filename")
+            return
+        try:
+            with open(f'folder/{filename}', 'r') as f:
+               
+                n = int(f.readline().strip())
+               
+                parents = list(map(int, f.readline().strip().split()))
+        except:
+            print("Error reading file.")
+            return
+    print("Tree Height:", compute_height(n, parents))
+
+sys.setrecursionlimit(10**7)  
+threading.stack_size(2**27)   
+threading.Thread(target=main).start() 
